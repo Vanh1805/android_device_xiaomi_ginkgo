c1b9a3b6af9362b30f91ad104259468c60506550
VanhNoob <59566412+VanhNoob@users.noreply.github.com>
2020-07-04 20:24:00 +0700

    Havoc

--- havoc_ginkgo.mk
+++ havoc_ginkgo.mk
@@ -12,13 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
-export CHERISH_BUILD_TYPE:= OFFICIAL
+export HAVOC_BUILD_TYPE:= VANH404
 
 # ANXCamera
 $(call inherit-product, vendor/ANXCamera/config.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
@@ -27,9 +27,10 @@ TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 SUPPORTS_GOOGLE_NEW_ASSISTANT := YES
 SUPPORTS_GOOGLE_ASSISTANT_EXTRA_RESOURCES := YES
+WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cherish_ginkgo
+PRODUCT_NAME := havoc_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
